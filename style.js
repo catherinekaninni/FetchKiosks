e3fd7f42837913b9f1e364e477094e2b806c1702
cathernekaninni <catherinekaninni@gmail.com>
2022-08-23 09:36:25 +0300

    changes

--- style.js
+++ style.js
@@ -1,107 +1,39 @@
-// server.js
-var http = require('http'); // Import Node.js core module
-var GreensKiosk = require('./GreensKiosk');
+fetch('http://localhost:5000/products/fruits')
 
-var server = http.createServer(function (req, res) {   // create web server
+.then(function (response) {
+    return response.json();
+})
+.then(function (data) {
+console.log(data);
 
-  // Set CORS headers
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Request-Method', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
-  res.setHeader('Access-Control-Allow-Headers', '*');
-  if (req.method === 'OPTIONS') {
-    res.writeHead(200);
-    res.end();
-    return;
-  }
-
-  if (req.url == '/') { // check the URL of the current request
-    // set response header
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    // set response content
-    res.write('<html><body><p>Greens Kiosk API.</p></body></html>');
-    res.end();
-  }
-  else if (req.url == "/products") { // check the URL of the current request
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.write(JSON.stringify(GreensKiosk.getItems()));
-    res.end();
-  }
-  else if (req.url == "/products/fruits") {
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.write(JSON.stringify(GreensKiosk.getItems('fruits')));
-    res.end();
-  }
-  else if (req.url == '/products/vegetables') {
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.write(JSON.stringify(GreensKiosk.getItems('vegetables')));
-    res.end();
-  }
-  else
-    res.end('Invalid Request!');
+let fruitList = document.getElementById('fruList');
+data.forEach(i=>{
+    let fruit = document.createElement('li');
+    fruit.innerText=`${i.name}`
+    fruitList.appendChild(fruit)
+})
+})
+.catch(function (error) {
+console.log('error', error);
 
 });
 
-server.listen(5000); // listen for any incoming requests
+fetch('http://localhost:5000/products/vegetables')
+.then(function (response) {
+    return response.json();
+})
+.then(function (data) {
+console.log(data);
 
-console.log('Node.js web server at port 5000 is running..')
-
-// GreensKiosk.js
-let products = [
-    {
-        name: 'Mangoes',
-        category: 'fruits'
-    },
-    {
-        name: 'Apples',
-        category: 'fruits'
-    },
-    {
-        name: 'Bananas',
-        category: 'fruits'
-    },
-    {
-        name: 'Oranges',
-        category: 'fruits'
-    },
-    {
-        name: 'Grapes',
-        category: 'fruits'
-    },
-    {
-        name: 'Kales',
-        category: 'vegetables'
-    },
-    {
-        name: 'Onions',
-        category: 'vegetables'
-    },
-    {
-        name: 'Tomatoes',
-        category: 'vegetables'
-    },
-    {
-        name: 'Cabbages',
-        category: 'vegetables'
-    },
-    {
-        name: 'Spinach',
-        category: 'vegetables'
-    },
-    ];
+let productList= document.getElementById('vegList');
+data.forEach(i=>{
+    let product = document.createElement('li');
+    product.innerText= `${i.name}`
+    productList.appendChild(product)
     
-    function getItems(category) {
-    if (category) {
-        let filteredProducts = products.filter(item => {
-            return item.category === category;
-        });
-        return filteredProducts;
-    }
-    return products;
-    }
+})
+})
+.catch(function (error) {
+console.log('error', error);
     
-    GreensKiosk = {
-    getItems: getItems
-    };
-    
-    module.exports = GreensKiosk;
+});
